cbe2590e11ced0346e653e28168b8b59dbc3ea1c
grigoreva13 <nastya130203@mail.ru>
2024-05-24 18:09:19 +0300

    First commit

--- Tree1.cpp
+++ Tree1.cpp
@@ -1,20 +1,110 @@
-﻿// Tree1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <unordered_map>
 
-#include <iostream>
+// Базовый класс для всех узлов AST
+struct Expression
+{
+    virtual Expression* transform(Transformer* tr) const = 0;
+    virtual ~Expression() {}
+};
+
+// Число
+struct Number : Expression
+{
+    Number(double value) : value_(value) {}
+    double value() const { return value_; }
+    virtual Expression* transform(Transformer* tr) const override { return tr->transformNumber(this); }
+private:
+    double value_;
+};
+
+// Бинарная операция
+struct BinaryOperation : Expression
+{
+    enum OpType { PLUS, MINUS, MUL, DIV };
+    BinaryOperation(Expression* left, OpType op, Expression* right) : left_(left), op_(op), right_(right) {}
+    Expression* left() const { return left_; }
+    OpType operation() const { return op_; }
+    Expression* right() const { return right_; }
+    virtual Expression* transform(Transformer* tr) const override { return tr->transformBinaryOperation(this); }
+private:
+    Expression* left_;
+    OpType op_;
+    Expression* right_;
+};
+
+// Вызов функции
+struct FunctionCall : Expression
+{
+    FunctionCall(std::string name, Expression* arg) : name_(name), arg_(arg) {}
+    std::string name() const { return name_; }
+    Expression* arg() const { return arg_; }
+    virtual Expression* transform(Transformer* tr) const override { return tr->transformFunctionCall(this); }
+private:
+    std::string name_;
+    Expression* arg_;
+};
+
+// Переменная
+struct Variable : Expression
+{
+    Variable(std::string name) : name_(name) {}
+    std::string name() const { return name_; }
+    virtual Expression* transform(Transformer* tr) const override { return tr->transformVariable(this); }
+private:
+    std::string name_;
+};
+
+// Класс-трансформер для копирования узлов AST
+class Transformer
+{
+public:
+    virtual ~Transformer() {}
+    virtual Expression* transformNumber(Number const* number) = 0;
+    virtual Expression* transformBinaryOperation(BinaryOperation const* binop) = 0;
+    virtual Expression* transformFunctionCall(FunctionCall const* fcall) = 0;
+    virtual Expression* transformVariable(Variable const* var) = 0;
+};
+
+// Класс, реализующий копирование узлов AST
+struct CopySyntaxTree : Transformer
+{
+    Expression* transformNumber(Number const* number) override
+    {
+        return new Number(number->value());
+    }
+
+    Expression* transformBinaryOperation(BinaryOperation const* binop) override
+    {
+        Expression* left = binop->left()->transform(this);
+        Expression* right = binop->right()->transform(this);
+        return new BinaryOperation(left, binop->operation(), right);
+    }
+
+    Expression* transformFunctionCall(FunctionCall const* fcall) override
+    {
+        Expression* arg = fcall->arg()->transform(this);
+        return new FunctionCall(fcall->name(), arg);
+    }
+
+    Expression* transformVariable(Variable const* var) override
+    {
+        return new Variable(var->name());
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Number* n32 = new Number(32.0);
+    Number* n16 = new Number(16.0);
+    BinaryOperation* minus = new BinaryOperation(n32, BinaryOperation::MINUS, n16);
+    FunctionCall* callSqrt = new FunctionCall("sqrt", minus);
+    Variable* var = new Variable("var");
+    BinaryOperation* mult = new BinaryOperation(var, BinaryOperation::MUL, callSqrt);
+    FunctionCall* callAbs = new FunctionCall("abs", mult);
+    CopySyntaxTree CST;
+    Expression* newExpr = callAbs->transform(&CST);
+
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
